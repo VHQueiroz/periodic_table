e89eee6e7829c9832ff06883a343dd1eb1121add
Victor Hugo Moreira Queiroz <vitorhugomq8@hotmail.com>
2025-04-02 02:31:54 +0000

    feat: if no query result

--- element.sh
+++ element.sh
@@ -8,3 +8,8 @@ then
 fi
 
 QUERY_RESULT=$($PSQL "SELECT atomic_number, name, symbol, type, atomic_mass, melting_point_celsius, boiling_point_celsius FROM elements INNER JOIN properties USING(atomic_number) INNER JOIN types USING(type_id) WHERE atomic_number='$1' OR symbol='$1' OR name='$1'")
+
+if [[ -z $QUERY_RESULT ]]
+then
+  echo "I could not find that element in the database."
+fi
