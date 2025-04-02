98d9e4307cfaa23e07fd972461cb4f518ba83454
Victor Hugo Moreira Queiroz <vitorhugomq8@hotmail.com>
2025-04-02 02:29:45 +0000

    feat: output query result

--- element.sh
+++ element.sh
@@ -6,3 +6,5 @@ then
   echo "Please provide an element as an argument."
   exit
 fi
+
+QUERY_RESULT=$($PSQL "SELECT atomic_number, name, symbol, type, atomic_mass, melting_point_celsius, boiling_point_celsius FROM elements INNER JOIN properties USING(atomic_number) INNER JOIN types USING(type_id) WHERE atomic_number='$1' OR symbol='$1' OR name='$1'")
